ccb1730b5c5a1a3a3c3499124e252f999d5f7851
Hiba Gohar <78848419+hibaagohar@users.noreply.github.com>
2023-10-07 15:07:48 +0400

    Added Header1.css Added Header1.jsx Added index.css Added LandingPage.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// Page Imports
+import LandingPage from './features/general/pages/LandingPage'
+ 
 function App() {
-    return (
-        <>
-        </>
-    );
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<LandingPage />} /> {/* Root URL when the app is accessed*/}
+          <Route path="/LandingPage" element={<LandingPage />} /> {/* To make sure this path is also valid, without the root path*/}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
