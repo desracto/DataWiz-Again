cc7082096368439431b906ee2a3048c0e0f04132
Hiba Gohar <78848419+hibaagohar@users.noreply.github.com>
2023-10-07 15:39:46 +0400

    Testing Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './assets/global.css';
 
 // Page Imports
 import LandingPage from './features/general/pages/LandingPage'
