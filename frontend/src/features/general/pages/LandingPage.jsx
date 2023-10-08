665f4b234fec5a6ae1f7e75b5c50909f34f7c049
Hiba Gohar <78848419+hibaagohar@users.noreply.github.com>
2023-10-08 19:00:27 +0400

    Added images

--- frontend/src/features/general/pages/LandingPage.jsx
+++ frontend/src/features/general/pages/LandingPage.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Header1 from '../../../global_components/Header1';
+import './LandingPage.css';
 
 function LandingPage() {
-
-    return (
-        <>
-        <Header1/>
-        </>
-    )
+  return (
+    <>
+      <Header1 />
+      <div className="IntroCard">
+        <div className="StyleVector1"></div>
+        <div className="UnleashThePowerOfSqlLearning">Unleash the<br />power of SQL Learning</div>
+        <div className="Description">
+          DataWiz is your gateway to mastering SQL effortlessly. Whether you're a curious learner or an educator looking for efficient teaching tools, DataWiz has something for everyone. Explore the world of interactive query animation and auto-grading quizzes for an engaging learning experience.
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default LandingPage
-
+export default LandingPage;
