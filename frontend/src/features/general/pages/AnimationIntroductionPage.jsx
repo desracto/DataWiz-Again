bad3b4cc61bf6cf1af0543e16919acb4b7e3f76c
M.Nihal <mnk665@uowmail.edu.au>
2023-11-13 00:06:47 +0400

    AnimationIntroductionPage is finished

--- frontend/src/features/general/pages/AnimationIntroductionPage.jsx
+++ frontend/src/features/general/pages/AnimationIntroductionPage.jsx
@@ -1,117 +1,111 @@
 import { useCallback } from "react";
+import { useNavigate} from "react-router-dom";
 import "./AnimationIntroductionPage.css";
-import vector5 from '../../../assets/images/vector-5.svg';
+import SecondHeader from "../../../global_components/SecondHeader";
+
+//Image imports
+import animQuery from '../../../assets/images/animInto-QueryAnimation.png';
+import vector62 from '../../../assets/images/vector-62.svg';
+import animSavedQuery from '../../../assets/images/animInto-savedQuery.png';
+import vector63 from '../../../assets/images/vector-63.svg';
+import animMain from '../../../assets/images/animInto-MainImage.png';
+
 
 const AnimationIntroductionPage = () => {
+  const navigate = useNavigate();
   const onAnimIntroSchemaAndAnimButtClick = useCallback(() => {
-    // Please sync "Schema Selection Page" to the project
+    navigate("/SchemaSelectionPage")
+  }, [navigate]);
+  const onAnimIntroSavedQueriesButtonClick = useCallback(() => {
+    navigate("/SchemaSelectionPage")
   }, []);
 
-  const onAnimIntroSavedQueriesButtonClick = useCallback(() => {
-    // Please sync "Schema Selection Page" to the project
-  }, []);
 
   return (
-    <div className="animation-introduction-page">
-      <div className="animation-introduction-page-child" />
-      <img
-        className="animation-introduction-page-item"
-        alt=""
-        src={vector5}
-      />
-      <img
-        className="animation-introduction-page-inner"
-        alt=""
-        src="/vector-6.svg"
-      />
-      <img
-        className="animation-introduction-page-child1"
-        alt=""
-        src="/vector-3.svg"
-      />
-      <div className="schema-selection-section">
-        <div className="schema-selection-section-child" />
-        <img
-          className="image-removebg-1-icon"
-          alt=""
-          src="/imageremovebg-1@2x.png"
-        />
-        <div className="schema-selection-section-inner">
-          <div className="animintro-schema-and-anim-butt-parent">
-            <button
-              className="animintro-schema-and-anim-butt"
-              onClick={onAnimIntroSchemaAndAnimButtClick}
-            >
-              <div className="animintro-schema-and-anim-butt-child" />
-              <div className="check-out-schemas">{`Check out Schemas & Animations`}</div>
-            </button>
-            <img className="group-item" alt="" src="/vector-61.svg" />
-            <div className="schema-selection-for-container">
-              <p className="schema-selection">Schema Selection</p>
-              <p className="schema-selection">For Query Animation</p>
-            </div>
+    <>
+      <SecondHeader/>
+        <div className="Anim-intro-sec1">
+          <img
+            className="image-removebg-preview-4-1"
+            alt=""
+            src={animMain}
+          />
+
+          <div className="Anim-intro-sec-Text">
+            <img 
+              className="group-child1" 
+              alt="" 
+              src={vector63} 
+            />
+            <div className="uncover-sql-magic">Uncover SQL Magic</div>
+            <div className="watch-queries-come">
+             Watch Queries Come Alive, Step by Step, in Engaging Animations.
+           </div>
+          </div>
+        </div>
+
+        <div className="Anim-intro-sec2">
+          <div className="Anim-intro-sec2-Text">
+          {/* <img className="group-item" alt="" src={vector61} /> */}
+            <p className="schema-selection">Schema Selection</p>
+            <p className="schema-selection">For <span className="schema-selection2">Query Animation</span></p>
+
             <div className="explore-the-intricate">
               Explore the intricate steps behind SQL queries with our Query
               Animation. Select from a range of schemas, craft your query, and
               witness a captivating animation that unveils how the database
               processes your query to derive the final result. An interactive
               and visual learning experience like never before.
+             </div>
+             <button
+              className="animintro-schema-and-anim-butt"
+              onClick={onAnimIntroSchemaAndAnimButtClick}
+              >
+              <div className="check-out-schemas">Check out Schemas & Animations</div>
+              </button>
             </div>
-          </div>
+          <img
+            className="image-removebg-1-icon"
+            alt=""
+            src={animQuery}
+          />    
         </div>
-      </div>
-      <div className="ellipse-div" />
-      <div className="saved-query-animation-section">
-        <div className="saved-query-animation-section-child" />
-        <img
-          className="saved-query-animation-section-item"
-          alt=""
-          src="/vector-62.svg"
-        />
-        <div className="saved-query-animations-parent">
-          <div className="saved-query-animations-container">
-            <p className="schema-selection">{`Saved Query `}</p>
-            <p className="schema-selection">Animations</p>
-          </div>
-          <div className="never-lose-your2">
-            Never lose your valuable query animations with our efficient saving
-            system. Access and manage all your previously animated queries in
-            one organized location. Re-view and analyze previous animations.
-            Download the animation for your records.
-          </div>
-          <button
-            className="animintro-saved-queries-button"
-            onClick={onAnimIntroSavedQueriesButtonClick}
-          >
-            <div className="animintro-schema-and-anim-butt-child" />
-            <div className="check-out-schemas">
-              Go to Saved Query Animations
+
+        <div className="Anim-intro-sec3">
+          <img
+            className="image-removebg-preview-3-1"
+            alt=""
+            src={animSavedQuery}
+          />
+          <div className="Anim-intro-sec3-text">
+            <img
+              className="saved-query-animation-section-item"
+              alt=""
+              src={vector62}
+            />
+              <p className="schema-selection">Saved Query </p>
+              <p className="schema-selection">Animations</p>
+              <div className="never-lose-your2">
+              Never lose your valuable query animations with our efficient saving
+              system. Access and manage all your previously animated queries in
+              one organized location. Re-view and analyze previous animations.
+              Download the animation for your records.
             </div>
-          </button>
-        </div>
-        <img
-          className="image-removebg-preview-3-1"
-          alt=""
-          src="/imageremovebgpreview-3-1@2x.png"
-        />
-      </div>
-      <div className="rectangle-group">
-        <div className="group-inner" />
-        <div className="vector-parent">
-          <img className="group-child1" alt="" src="/vector-63.svg" />
-          <div className="uncover-sql-magic">Uncover SQL Magic</div>
-          <div className="watch-queries-come">
-            Watch Queries Come Alive, Step by Step, in Engaging Animations.
+            <button
+                className="animintro-saved-queries-button"
+                onClick={onAnimIntroSavedQueriesButtonClick}
+            >
+                <div className="check-out-schemas">
+                  Go to Saved Query Animations
+                </div>
+            </button>
           </div>
         </div>
-        <img
-          className="image-removebg-preview-4-1"
-          alt=""
-          src="/imageremovebgpreview-4-1@2x.png"
-        />
-      </div>
-    </div>
-  );
-};
+      
+
+
+    </>
+  )
+}
 
 export default AnimationIntroductionPage;
