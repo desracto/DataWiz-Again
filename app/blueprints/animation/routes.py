1820556a32d3dbad2c0c884ca0c3c7c64ae51870
rida-asif <rfa373@uowmail.edu.au>
2023-12-02 17:59:28 +0400

    updated results in routes

--- app/blueprints/animation/routes.py
+++ app/blueprints/animation/routes.py
@@ -15,7 +15,7 @@ from ._prefixed_models import Schema5_Album as Album, Schema5_Artist as Artist, 
 
 from pyparsing import ParseException
 
-from app.scripts.active_animation_parser import generate_animation_steps
+from app.scripts.Active_animation_parser import generate_animation_steps
 from app.scripts.Active_sqlra import translate_query
 
 @animation_bp.route('/schema/1/')
@@ -32,9 +32,11 @@ def schema1():
         emps_json.append(emp.as_dict())
     
     results = {
-        "employees": emps_json
+        0: {
+            'table_name': 'Employees',
+            'data': emps_json
+        },
     }
-    
     return jsonify(results=results)
 
 @animation_bp.route('/schema/2/')
@@ -85,9 +87,15 @@ def schema3():
         enrolls_json.append(enroll.as_dict())
 
     results = {
-        'course': course_json,
-        'enrollments': enrolls_json
-    } 
+        0: {
+            'table_name': 'Courses',
+            'data': course_json
+        },
+        1: {
+            'table_name': 'Enrollment',
+            'data': enrolls_json
+        }
+    }
 
     return jsonify(results=results)
 
@@ -114,9 +122,18 @@ def schema4():
         tickets_json.append(ticket.as_dict())
 
     results = {
-        'flight': flights_json,
-        'passenger': passengers_json,
-        'ticket': tickets_json
+        0: {
+            'table_name': 'Flight',
+            'data': flights_json
+        },
+        1: {
+            'table_name': 'Passenger',
+            'data': passengers_json
+        },
+        2: {
+            'table_name': 'Tickets',
+            'data': tickets_json
+        }
     }
 
     return jsonify(results=results)
@@ -148,10 +165,22 @@ def schema5():
         artists_json.append(artist.as_dict())
 
     results = {
-        'album': albums_json,
-        'genre': genres_json,
-        'song': songs_json,
-        'artist': artists_json
+        0: {
+            'table_name': 'Albums',
+            'data': albums_json
+        },
+        1: {
+            'table_name': 'Genres',
+            'data': genres_json
+        },
+        2: {
+            'table_name': 'Songs',
+            'data': songs_json
+        },
+        3: {
+            'table_name': 'Artists',
+            'data': artists_json
+        }
     }
 
     return jsonify(results=results)
