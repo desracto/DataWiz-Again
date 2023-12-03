c99de1d02c9b22f5aa7022b50fda93cf18070dce
M.Nihal <mnk665@uowmail.edu.au>
2023-12-03 20:20:17 +0400

    fixed acccount settings

--- frontend/src/features/general/pages/AccountSettingsPage.jsx
+++ frontend/src/features/general/pages/AccountSettingsPage.jsx
@@ -24,20 +24,19 @@ const AccountSettingPage = ({request}) => {
     //loading user data 
     useEffect(() => {
         const fetchUserData = async () => {
+            
             try {
-                // Replace the following URL with your actual API endpoint to fetch user data
                 request({
                     url:"api/user/load_user/",
                     method: 'get',
                     credentials: 'include'})
-                .then(response => {
-                    userData = response.data;
+                .then(response => {  
+                    console.log(response.data);
+                    setValue("fullName", response.data.fullName);
+                    setValue("username", response.data.username);
+                    setValue("email", response.data.email)
 
-                    setValue("fullName", userData.fullName);
-                    setValue("username", userData.username);
-                    setValue("email", userData.email)
-
-                    setUserData(userData)})
+                    })
                 .catch(error => {
                     console.error(error.response)})
 
