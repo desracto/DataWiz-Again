efd409f0803dcbc55a985b00df29a500d674405b
rida-asif <rfa373@uowmail.edu.au>
2023-12-07 01:48:52 +0400

    updated the pdf and grader code

--- test/grader.py
+++ test/grader.py
@@ -19,7 +19,7 @@ def major_minor(dict1, dict2):
         if keyword_unsimilar == True:
             if set(dict1[key]) != set(dict2[key]):
                 error_list.append({key: (dict1[key], dict2[key])})
-                print("Mismatch of value count as per original query")
+                # print("Mismatch of value count as per original query")
             else: 
                 for item1, item2 in zip(dict1[key], dict2[key]):
                     # print(f'item1:  {item1} and item2 key: {item2}')
@@ -28,7 +28,7 @@ def major_minor(dict1, dict2):
         else: 
             if set(dict1[key]) != set(dict2[key]):
                 error_list.append({key: (dict1[key], dict2[key])})
-                print("Mismatch of value count as per original query")
+                # print("Mismatch of value count as per original query")
             else: 
                 for item1, item2 in zip(dict1[key], dict2[key]):
                     # print(f'item1:  {item1} and item2 key: {item2}')
@@ -46,30 +46,67 @@ def major_minor(dict1, dict2):
     # print(minor_mistakes)
 
     score = 1 - (major_mistakes + minor_mistakes)
-    print("\n\n")
-    print("Major mistakes: ")
-    print(keyword_list)
-    print("\nMajor mistakes: ")
-    print(error_list)
-    print(f'\nFinal score: {score}')
+    # print("\n\n")
+    # print("Major mistakes: ")
+    # print(keyword_list)
+    # print("\nMinor mistakes: ")
+    # print(error_list)
+    # print(f'\nFinal score: {score}')
 
-    return score
+    print(keyword_list)
+
+    major_string = ""
+    if len(keyword_list) > 0:
+        for i in keyword_list:
+            major_string += str(i) + "\n"
+    else:
+        major_string = "None"
+
+    minor_string = ""
+    if len(error_list) > 0:
+        for i in error_list:
+            minor_string += str(i) + "\n"
+    else:
+        minor_string = "None"
+    
+    explanation = f"\nMissing Keywords: \n{major_string}\nMinor mistakes: \n{minor_string}\nScore: {score}\n"
+    print(explanation)
+
+    return score, explanation
 
 
 
 def spellchecker(teacher_sql, student_sql):
 
+    mistake_count = 0
+    keyword_list = []
+    error_list = []
+    keyword_count = 0
     for key in teacher_sql:
         if key in student_sql:
             teacher_values = teacher_sql[key]
             student_values = student_sql[key]
-            print(f"Comparison for '{key}':")
+            # print(f"Comparison for '{key}':")
             for t_value in teacher_values:
                 for s_value in student_values:
                     distance = textdistance.levenshtein.normalized_distance(t_value, s_value)
-                    print(f"Distance between '{t_value}' and '{s_value}': {distance}")
+                    keyword_count =+ 1
+                    if distance >= 0.35 and distance <= 0.8:
+                        mistake_count =+ 1
+                        keyword_list.append(key)
+                        error_list.append((t_value, s_value))
+                        # print(f"Distance between '{t_value}' and '{s_value}': {distance}")
 
-    return None
+    mistake_text = "\nSpelling errors:"
+    mistake_string = ""
+    for i in range(0, len(keyword_list)):
+        mistake_string += "\nKeyword: "+ str(keyword_list[i]) + "\n" + str(error_list[i])
+
+    mistake_text += mistake_string
+
+    score = 1 - (mistake_count/keyword_count)
+
+    return score, mistake_text
 
 
 # def get_join_types(sql_dict):
@@ -144,6 +181,57 @@ def check_unnecessary_additions(teacher_sql, student_sql):
     return unnecessary_additions
 
 
+def auto_grader(correct_ans, stu_ans):
+    
+    sql_c = translate_query(query = correct_ans,
+                                DEBUG=True,
+                                CLEAN=True)
+    sql_s = translate_query(query = stu_ans,
+                                DEBUG=True,
+                                CLEAN=True)
+    
+    print(f'SQL from teacher: {sql_c}')
+    print(f'SQL from student: {sql_s}\n')
+
+    filters = {
+        # "major_and_minorf": True,
+        "spell_typo_checkerf": True,
+        "join_checkerf" : True,
+        "additional_checkerf" : True
+    }
+
+    score_counter = 1
+    temp_score = 0
+
+    score1, result = major_minor(sql_c, sql_s)
+    temp_score += score1
+
+    if filters.get("spell_typo_checkerf"):
+        score_counter =+ 1
+        score2, temp_result2 = spellchecker(sql_c, sql_s)
+        temp_score += score2
+        result += temp_result2
+
+    # join_types = ['INNER JOIN', 'LEFT JOIN', 'LEFT OUTER JOIN', 'RIGHT JOIN', 'RIGHT OUTER JOIN', 'FULL JOIN', 'FULL OUTER JOIN', 'JOIN', 'NATURAL JOIN', 'SELF JOIN']
+    # join_check = any(key in sql_c for key in join_types)
+
+    # if join_check and filters.get("join_checkerf"):
+    #     join_type_difference = compare_join_types(sql_c, sql_s)
+    #     if join_type_difference is not None:
+    #         score_countr =+ 1
+    #         print(f"Deduction: {join_type_difference}")
+
+    # if filters.get("additional_checkerf"):
+        # score_countr =+ 1
+        # score3, temp_result3 = check_unnecessary_additions(sql_c, sql_s)
+        # temp_score += score3
+        # result += temp_result3
+
+    score = temp_score/score_counter
+
+    return score, result
+    # return None
+
 def main():
     
     # correct_ans = "SELECT student.name FROM students WHERE grade >= 90"
@@ -155,11 +243,11 @@ def main():
     # correct_ans = "SELECT name, inspiration FROM programme INNER JOIN scores ON programme.id = score.id WHERE s.inspiration > 18 GROUP BY id HAVING something"
     # stu_ans = "SELECT name FROM programme INNER JOIN scores ON programme.id = score.id WHERE s.inspiration > 18 GROUP BY id HAVING something"
     
-    # correct_ans = "SELECT employees, employee_id FROM employees RIGHT JOIN departments ON employees.department_id = departments.department_id where employees.employee_id = 100"
-    # stu_ans = "SELECT employees, employee_id FROM emp LEFT JOIN departments ON employees.department_id = departments.department_id where employees.employee_id > 100"
+    # correct_ans = "SELECT employees, employee_id FROM employees INNER JOIN departments ON employees.department_id = departments.department_id where employees.employee_id = 100"
+    # stu_ans = "SELECT employees, employee_id FROM emp JOIN departments ON employees.department_id = departments.department_id where employees.employee_id > 100"
 
-    # correct_ans = "SELECT first_name, last_name FROM employees WHERE department = 'Sales' GROUP BY department HAVING COUNT(*) > 5 ORDER BY last_name ASC LIMIT 10"
-    # stu_ans = "SELECT first_name, last_name FROM employees WHERE department = 'Sales' GROUP BY department LIMIT 10"
+    correct_ans = "SELECT first_name, last_name FROM employees WHERE department = 'Sales' GROUP BY department HAVING COUNT(*) > 5 ORDER BY last_name ASC LIMIT 10"
+    stu_ans = "SELECT first_name, last_name FROM employees WHERE department = 'Sales' GROUP BY department LIMIT 10"
    
     # correct_ans = "SELECT firstname, lastname FROM employees WHERE dept = 'Sales' GROUP BY dept HAVING COUNT(*) > 5 ORDER BY lastname ASC LIMIT 10"
     # stu_ans = "SELECT first_name, last_name FROM employees WHERE department = 'Sales' GROUP BY department LIMIT 10"
@@ -167,35 +255,27 @@ def main():
     # correct_ans = "SELECT professor_name FROM courses WHERE department = 'Computer Science'"
     # stu_ans = "SELECT subject, professor_name FROM classes WHERE field = 'CS' and department = 'Computer Science'"
 
-    correct_ans = "SELECT professor_name FROM courses WHERE department = 'Computer Science'"
-    stu_ans = "SELECT subject, professor_name FROM classes WHERE field = 'CS' and department = 'Computer Science'"
+    # correct_ans = "SELECT professor_name FROM courses WHERE department = 'Computer Science'"
+    # stu_ans = "SELECT subject, professor_name FROM classes WHERE field = 'CS' and department = 'Computer Science'"
 
     # correct_ans = "SELECT employees.employee_id, employees.employee_name, departments.department_name FROM employees, departments WHERE employees.department_id = departments.department_id"
     # stu_ans = "SELECT employees.employee_id, employees.employee_name, departments.department_name FROM employees JOIN departments ON employees.department_id = departments.department_id"
 
-    sql_c = translate_query(query = correct_ans,
-                                DEBUG=True,
-                                CLEAN=True)
-    sql_s = translate_query(query = stu_ans,
-                                DEBUG=True,
-                                CLEAN=True)
-    
-    print(f'SQL from teacher: {sql_c}')
-    print(f'SQL from student: {sql_s}\n')
+    # correct_ans = "SELECT name, CASE WHEN age < 30 THEN 'Young' ELSE 'Old' END AS age_group FROM employees"
+    # stu_ans = "SELECT name, CASE WHEN age < 30 THEN 'Young' ELSE 'Old' END AS age_group"
 
-    major_minor(sql_c, sql_s)
+    # correct_ans = "SELECT sum(salary) AS sum FROM employees WHERE name LIKE '%son%';"
+    # stu_ans = "SELECT count(salary) AS count FROM employees WHERE name LIKE '%son%';"
 
-    # spellchecker(sql_c, sql_s)
+    # correct_ans = "SELECT album_id, album_name FROM employees WHERE name LIKE '%son%';"
+    # stu_ans = "SELECT album.album_id FROM employees WHERE name LIKE '%son%';"
 
-    # join_types = ['INNER JOIN', 'LEFT JOIN', 'LEFT OUTER JOIN', 'RIGHT JOIN', 'RIGHT OUTER JOIN', 'FULL JOIN', 'FULL OUTER JOIN', 'JOIN', 'NATURAL JOIN', 'SELF JOIN']
-    # join_check = any(key in sql_c for key in join_types)
+    # correct_ans = "SELECT * FROM employees WHERE age IN (SELECT MAX(age) FROM employees)"
+    # stu_ans = "SELECT * FROM employees WHERE age IN (SELECT age FROM employees)"
+    auto_grader(correct_ans, stu_ans)
 
-    # if join_check:
-    #     join_type_difference = compare_join_types(sql_c, sql_s)
-    #     if join_type_difference is not None:
-    #         print(f"Deduction: {join_type_difference}")
+    return None
 
-    # check_unnecessary_additions(sql_c, sql_s)
 
 if __name__ == "__main__":
     main()
--- test/pdf_generate.py
+++ test/pdf_generate.py
@@ -1,56 +1,192 @@
-# from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text
 from fpdf import FPDF, XPos, YPos
+from grader import *
+import time
 
-# engine = create_engine("mysql+mysqlconnector://sql12658112:yFlLxXMggz@sql12.freemysqlhosting.net/sql12658112")
+# engine = create_engine("mysql+pymysql://sql12658112:yFlLxXMggz@sql12.freemysqlhosting.net/sql12658112")
+# connection = engine.connect()
+
+
+start_time = time.time()
+
+c_queries = [
+    "SELECT * FROM employees;",
+    "SELECT name, age, salary FROM employees WHERE department_id = 10;",
+    "SELECT DISTINCT department_id FROM employees;",
+    "SELECT name, MAX(salary) AS max_salary FROM employees GROUP BY department_id;",
+    "SELECT name FROM employees WHERE age > 30;",
+    "SELECT * FROM employees ORDER BY salary DESC;",
+    "SELECT name, SUM(salary) AS total_salary FROM employees GROUP BY department_id;",
+    "SELECT * FROM employees WHERE name LIKE 'A%';",
+    "SELECT * FROM employees WHERE department_id IN (1, 3, 5);",
+    "SELECT AVG(salary) AS avg_salary FROM employees WHERE department_id = 10;",
+    "SELECT COUNT(*) AS total_employees FROM employees;",
+    "SELECT * FROM employees WHERE age BETWEEN 25 AND 35;",
+    "SELECT department_id, COUNT(*) AS num_employees FROM employees GROUP BY department_id HAVING num_employees > 5;",
+    "SELECT * FROM employees WHERE salary > (SELECT AVG(salary) FROM employees);",
+    "SELECT name, RANK() OVER (ORDER BY salary DESC) AS rank FROM employees;",
+    "SELECT * FROM employees LEFT JOIN departments ON employees.department_id = departments.id;",
+    "SELECT name, COALESCE(salary, 0) AS salary FROM employees;",
+    "SELECT name, CASE WHEN age < 30 THEN 'Young' ELSE 'Old' END AS age_group FROM employees;",
+    "SELECT department_id, PERCENT_RANK() OVER (ORDER BY salary) AS salary_percent_rank FROM employees;",
+    "SELECT name, LEAD(salary) OVER (ORDER BY salary) AS next_salary FROM employees;"
+    "SELECT name, COUNT(*) AS num_employees FROM employees GROUP BY name HAVING num_employees > 1;",
+    "SELECT * FROM employees WHERE salary NOT BETWEEN 30000 AND 60000;",
+    "SELECT AVG(salary) AS avg_salary FROM employees WHERE department_id = 5;",
+    "SELECT department_id, MAX(salary) AS max_salary FROM employees GROUP BY department_id HAVING MAX(salary) > 80000;",
+    "SELECT * FROM employees WHERE name LIKE '%son%';",
+    "SELECT name, DENSE_RANK() OVER (ORDER BY salary DESC) AS dense_rank FROM employees;",
+    "SELECT name, COALESCE(salary, 0) AS salary FROM employees;",
+    "SELECT department_id, SUM(CASE WHEN salary > 50000 THEN 1 ELSE 0 END) AS high_salary_employees FROM employees GROUP BY department_id;",
+    # "SELECT * FROM employees WHERE age IN (SELECT MIN(age) FROM employees);",
+    "SELECT name, NTILE(4) OVER (ORDER BY salary) AS quartile FROM employees;"
+]
+
+# Modified SQL queries list with intentional mistakes or variations
+stu_queries = [
+    "SELECT * FORM employees;",  # Missing keyword 'FROM'
+    "SELECT name, age salary FROM employees WHERE department_id = 10;",  # Missing comma between 'age' and 'salary'
+    "SELECT DISTINCT department_id FROM employees WHERE name IS NOT NULL;",  # Added condition 'WHERE name IS NOT NULL'
+    "SELECT name, MAX(salary) AS highest_salary FROM employees GROUP BY department_id;",  # Changed alias to 'highest_salary'
+    "SELECT names FROM employees WHERE age > 30;",  # Changed column name to 'names'
+    "SELECT * FROM employees ORDER salary DESC;",  # Missing keyword 'BY' in ORDER BY
+    "SELECT name, SUM(salary) AS total_salary FROM employees WHERE department_id = 2;",  # Changed department_id value
+    "SELECT * FROM employees WHERE name LIKE '%A%';",  # Changed LIKE pattern
+    "SELECT * FROM employees WHERE department_id IN 1, 3, 5;",  # Missing parentheses in IN statement
+    "SELECT AVG(salary) avg_salary FROM employees WHERE department_id = 10;",  # Changed alias position
+    "SELECT COUNT(*) total_employees FROM employees WHERE age BETWEEN 25 OR 35;",  # Incorrect BETWEEN syntax
+    "SELECT department_id, COUNT(*) AS num_employees FROM employees GROUP BY department_id HAVING num_employees > 8;",  # Changed HAVING condition
+    "SELECT * FROM employees WHERE salary > (SELECT AVG(salary) FROM employees) AND age < 30;",  # Added an additional condition
+    "SELECT name, RANK() FROM employees ORDER BY salary DESC;",  # Removed OVER clause in window function
+    "SELECT * FROM employees RIGHT JOIN departments ON employees.department_id = departments.id;",  # Changed LEFT JOIN to RIGHT JOIN
+    "SELECT name, COALESCE(salary) AS salary FROM employees;",  # Removed the default value in COALESCE
+    "SELECT name, CASE WHEN age < 30 THEN 'Young' END AS age_group FROM employees;",  # Changed CASE statement
+    "SELECT department_id, PERCENT_RANK() AS salary_percent_rank FROM employees ORDER BY salary;",  # Removed OVER clause
+    "SELECT name, LEAD(salary) FROM employees ORDER BY salary;",  # Removed OVER clause in window function
+    "SELECT name, COUNT(*) AS num_employees FROM employees GROUP BY name HAVING num_employees > 3;",  # Changed HAVING condition
+    "SELECT * FORM employees WHERE salary BETWEEN 30000 AND 60000;",  # Missing keyword 'FROM'
+    "SELECT AVG(salary) AS avg_sal FROM employees WHERE department_id 5;",  # Missing operator in WHERE clause
+    "SELECT department_id, MAX(salary) AS max_salary FROM employees WHERE salary > 70000 GROUP BY department_id;",  # Added condition to WHERE clause
+    "SELECT * FROM employees WHERE name LIKE '%on%';",  # Changed LIKE pattern
+    "SELECT name, DENSE_RANK() OVER (ORDER BY salary) AS dense_rank FROM employees WHERE age > 25;",  # Changed WHERE condition
+    "SELECT name, COALESCE(salary, 0) AS salary FROM employees WHERE age > 30;",  # Changed WHERE condition
+    "SELECT department_id, SUM(CASE WHEN salary > 50000 THEN 1 END) AS high_salary_employees FROM employees GROUP BY department_id;",  # Removed ELSE in CASE statement
+    # "SELECT * FROM employees WHERE age IN (SELECT MAX(age) FROM employees);",  # Changed subquery to MAX(age)
+    "SELECT name, NTILE(4) AS quartile FROM employees ORDER BY salary DESC;"  # Removed OVER clause
+]
+
+end_time = time.time()
+
+print(f"Total time taken for grader: {end_time - start_time}")
+
+title = "Quiz Results"
 
 class PDF(FPDF):
-    def header(self, title):
-        self.image('DataWiz-Logo.png', 10, 8, 20)
-        self.set_font('helvetica', 'B', 20)
+
+    def set_page_margin(self):
+        self.set_margin(20)
+    
+
+    def header(self):
+        self.image(r'C:\Users\ASIF\DataWiz\general\frontend\src\assets\images\DataWiz-Logo.png', 10, 8, 16)
+        self.set_font('Gilroy-Bold', '', 20)
 
         title_w = self.get_string_width(title) + 6
         doc_w = self.w
         self.set_x((doc_w - title_w)/2)
 
+        self.set_line_width(1)
         self.set_text_color(220, 50, 50)
 
-        self.set_line_width(1)
-
         self.cell(title_w, 10, title, align='C')
-        self.ln(20)
+        self.ln(30)
 
 
     def footer(self):
         self.set_y(-15)
-        self.set_font('helvetica', 'I', 10)
+        self.set_font('Gilroy-MediumItalic', '', 10)
         self.set_text_color(169, 169, 169)
         self.cell(0, 10, f'Page {self.page_no()}', align='C')
 
+    def question(self, num, qs):
+        self.set_font('Gilroy-SemiBold', '', 14)
+        self.set_fill_color(255, 244, 214)
 
-    def chapter_title(self, ch_num, ch_title):
-        self.set_font('helvetica', '', 12)
+        length = len(str(num)) + 70
+        question = f'Question {num}'
+        self.cell(length, 8, question, new_x="LMARGIN", new_y="NEXT", fill=1)
+        
+        self.set_font('Gilroy-Regular', '', 12)
         self.set_fill_color(200, 220, 255)
-        chapter_title = f'Chapter {ch_num}: {ch_title}'
-        self.cell(0, 5, chapter_title, ln=1, fill=1)
+        question = f'{qs}'
+        self.multi_cell(0, 5, question, new_x="LMARGIN", new_y="NEXT", fill=1)
         self.ln()
 
+    def teacher_ans(self, question_answer):
+        student_ans = f"Correct Answer: "
+        self.set_font('Gilroy-Light', '', 11)
+        self.cell(0, 7, student_ans, new_x="LMARGIN", new_y="NEXT")
 
-    def chapter_body(self, name):
-        with open(name, 'rb') as fh:
-            txt = fh.read().decode('latin-1')
-
-        self.set_font('times', '', 12)
-        self.multi_cell(0, 5, txt)
+        self.set_font('Gilroy-Regular', '', 13)
+        self.multi_cell(0, 6, question_answer, new_x="LMARGIN", new_y="NEXT")
         self.ln()
-        self.set_font('times', 'I', 12)
-        self.multi_cell(0, 5, 'End of chapter!')
+    
+    def student_ans(self, question_answer):
+        student_ans = f"Student Answer: "
+        self.set_font('Gilroy-Light', '', 11)
+        self.cell(0, 7, student_ans, new_x="LMARGIN", new_y="NEXT")
+        
+        self.set_font('Gilroy-Regular', '', 13)
+        # self.set_fill_color(169, 169, 169)
+        self.multi_cell(0, 6, question_answer, new_x="LMARGIN", new_y="NEXT", fill=0)
+        self.ln()
 
+    def autograding_results(self, result):
+        autograder_text = f"Autograding Results: "
+        self.set_font('Gilroy-Light', '', 11)
+        self.cell(0, 7, autograder_text, new_x="LMARGIN", new_y="NEXT")
+        
+        self.set_font('Gilroy-Regular', '', 12)
+        self.set_fill_color(221, 221, 221)
+        self.multi_cell(0, 6, result, new_x="LMARGIN", new_y="NEXT", fill=1)
+        self.ln()
 
+    def print_qs_set(self, ques_num, ques, ques_ans, stu_ans, result):
+        self.question(ques_num, ques)
+        self.ln()
+        self.teacher_ans(ques_ans)
+        self.student_ans(stu_ans)
+        self.ln()
+        self.autograding_results(result)
+        self.ln()
 
-    def print_chapter(self, ch_num, ch_title, name):
-        self.add_page()
-        self.chapter_title(ch_num, ch_title)
-        self.chapter_body(name)
+        
 
 
 pdf = PDF('P', 'mm', 'A4')
+
+pdf.add_font('Gilroy-Black', '', r'C:\Users\ASIF\DataWiz\general\frontend\src\assets\fonts\Gilroy-Black.ttf')
+pdf.add_font('Gilroy-Bold', '', r'C:\Users\ASIF\DataWiz\general\frontend\src\assets\fonts\Gilroy-Bold.ttf')
+pdf.add_font('Gilroy-ExtraBold', '', r'C:\Users\ASIF\DataWiz\general\frontend\src\assets\fonts\Gilroy-ExtraBold.ttf')
+pdf.add_font('Gilroy-Heavy', '', r'C:\Users\ASIF\DataWiz\general\frontend\src\assets\fonts\Gilroy-Heavy.ttf')
+pdf.add_font('Gilroy-Light', '', r'C:\Users\ASIF\DataWiz\general\frontend\src\assets\fonts\Gilroy-Light.ttf')
+pdf.add_font('Gilroy-Medium', '', r'C:\Users\ASIF\DataWiz\general\frontend\src\assets\fonts\Gilroy-Medium.ttf')
+pdf.add_font('Gilroy-MediumItalic', '', r'C:\Users\ASIF\DataWiz\general\frontend\src\assets\fonts\Gilroy-MediumItalic.ttf')
+pdf.add_font('Gilroy-Regular', '', r'C:\Users\ASIF\DataWiz\general\frontend\src\assets\fonts\Gilroy-Regular.ttf')
+pdf.add_font('Gilroy-SemiBold', '', r'C:\Users\ASIF\DataWiz\general\frontend\src\assets\fonts\Gilroy-SemiBold.ttf')
+pdf.add_font('Gilroy-Thin', '', r'C:\Users\ASIF\DataWiz\general\frontend\src\assets\fonts\Gilroy-Thin.ttf')
+
+pdf.add_page()
+pdf.set_page_margin()
+pdf.set_auto_page_break(auto = True, margin = 15)
+
+for i in range(15, len(stu_queries)):
+    score, result = auto_grader(c_queries[i], stu_queries[i])
+    print(result)
+    pdf.print_qs_set(i, i, c_queries[i], stu_queries[i], result)
+    if i == 25:
+        break
+
+
+
+pdf.output('quiz_try.pdf')
