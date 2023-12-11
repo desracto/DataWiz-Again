525bccbb9b9d5d4f3e221b2d91667a5151169c5a
Hiba Gohar <78848419+hibaagohar@users.noreply.github.com>
2023-12-11 13:47:35 +0400

    Added comments on all Animation front-end pages

--- frontend/src/features/animation/pages/SavedAnimationsPage.jsx
+++ frontend/src/features/animation/pages/SavedAnimationsPage.jsx
@@ -5,97 +5,104 @@ import { useNavigate } from 'react-router-dom';
 import "./SavedAnimationsPage.css";
 import SecondHeader from '../../../global_components/SecondHeader';
 
-
+// Define the SavedAnimationsPage functional component
 function SavedAnimationsPage() {
+  // Use the navigate hook for programmatic navigation
   const navigate = useNavigate();
+
+  // Retrieve saved animations from localStorage or initialize an empty array
   const savedAnimations = JSON.parse(localStorage.getItem('animations') || '[]');
+
+  // Function to delete the most recent animation
   const deleteMostRecentAnimation = () => {
     if (savedAnimations.length === 0) {
       alert("No animations to delete.");
       return;
     }
 
+    // Create a new array excluding the most recent animation
     const updatedAnimations = savedAnimations.slice(0, savedAnimations.length - 1);
+
+    // Update the animations in localStorage
     localStorage.setItem('animations', JSON.stringify(updatedAnimations));
-    window.location.reload(); // Refresh the page to update the UI
+
+    // Refresh the page to update the UI
+    window.location.reload();
   };
-  
+
+  // Function to navigate to a saved animation page
   const goToSavedAnimation = (animationId) => {
-    // Navigate to the UNCompletedQuiz page with the quiz ID as a parameter
+    // Navigate to the UNCompletedQuiz page with the animation ID as a parameter
     navigate(`/uncompleted-quiz/${animationId}`);
   };
 
   return (
     <>
-    <SecondHeader/>
-    <div className="main-container">
+      <SecondHeader />
+      {/* Main container for the SavedAnimationsPage */}
+      <div className="main-container">
+        {/* Heading container for saved animations */}
         <div className="headingcontainer-savedquizzes">
-            <div className="heading-subcontainer">
-                <h1>Saved Animation</h1>
-            
-            </div>
-            
+          <div className="heading-subcontainer">
+            <h1>Saved Animation</h1>
+          </div>
         </div>
-        <div className="content-container ">
-            <div className="content_heading">
+
+        {/* Content container for saved animations */}
+        <div className="content-container">
+          {/* Heading for recent animations with a delete button */}
+          <div className="content_heading">
             <span>Recent Animations </span>
-            <FaTrash 
-                    color="#98989F" 
-                    size={24} 
-                    style={{cursor: "pointer"}}
-                    onClick={deleteMostRecentAnimation}
-                />
-            
-            </div>
-            {savedAnimations.map((animation, index) => (
+            <FaTrash
+              color="#98989F"
+              size={24}
+              style={{ cursor: "pointer" }}
+              onClick={deleteMostRecentAnimation}
+            />
+          </div>
+
+          {/* Map through and display each saved animation */}
+          {savedAnimations.map((animation, index) => (
             <div key={animation.id} className="animation-content_card">
-            <div className="content-card-button-div ">
-                    
+              <div className="content-card-button-div">
+                {/* Display animation details */}
                 <span> {animation.name}</span>
                 <small>Date: {animation.date}</small>
-                <small>Time: {animation.time}</small> 
-               
-            
-                </div>
-                
-            <FaChevronRight style={{cursor:"pointer"}} 
-            onClick={() => goToSavedAnimation(animation.id)}
-            />
-            
+                <small>Time: {animation.time}</small>
+              </div>
+
+              {/* Navigate to the saved animation on clicking the right arrow icon */}
+              <FaChevronRight
+                style={{ cursor: "pointer" }}
+                onClick={() => goToSavedAnimation(animation.id)}
+              />
             </div>
-            ))}
-            
-            
-            <div className="content_heading">
+          ))}
+
+          {/* Heading for all animations */}
+          <div className="content_heading">
             <span1>All Animations</span1>
-            </div>
+          </div>
 
-           
-            {savedAnimations.map((animation, index) => (
+          {/* Map through and display all saved animations */}
+          {savedAnimations.map((animation, index) => (
             <div key={animation.id} className="animation-content_card">
-            <div className="content-card-button-div ">
-                    
+              <div className="content-card-button-div">
+                {/* Display animation details */}
                 <span> {animation.name}</span>
                 <small>Date: {animation.date}</small>
-                <small>Time: {animation.time}</small> 
-               
-            
-                </div>
-                
-            <FaChevronRight style={{cursor:"pointer"}} 
-            onClick={() => goToSavedAnimation(animation.id)}
-            />
-            
+                <small>Time: {animation.time}</small>
+              </div>
+
+              {/* Navigate to the saved animation on clicking the right arrow icon */}
+              <FaChevronRight
+                style={{ cursor: "pointer" }}
+                onClick={() => goToSavedAnimation(animation.id)}
+              />
             </div>
-            ))}
-
-            
-
-
-           
-            
+          ))}
         </div>
-    </div>
+      </div>
     </>
   );
 }
