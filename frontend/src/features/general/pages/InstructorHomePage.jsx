760c7cc14612977f859d2730573157b3f6fbef39
Hiba Gohar <78848419+hibaagohar@users.noreply.github.com>
2023-12-12 02:50:46 +0400

    Added svgs

--- frontend/src/features/general/pages/InstructorHomePage.jsx
+++ frontend/src/features/general/pages/InstructorHomePage.jsx
@@ -17,7 +17,7 @@ import CreateQuizzIcon from '../../../assets/images/Create-Quizzes-Logo.png';
 const request = axios.create({
     baseURL: "http://localhost:5000",
     headers: {
-        "Content-Type" : "application/json"
+        "Content-Type": "application/json"
     },
     withCredentials: true,
     timeout: 300000
@@ -70,141 +70,141 @@ const InstructorHomePage = () => {
         navigate("/SavedQuizzesPage");
     }, [navigate]);
 
-  return (
-    <>
-    <SecondHeader/>
-    <img
-        className="Stacked-Waves"
-        alt=""
-        src={stackedWaves}
-    />
-    <div className="Top-Section">
-
-        <div className="IHome-Page-Cont1">
-
-            <div className="your-gateway-to">
-                {`Your Gateway to Mastering SQL Queries with Query Visualization, and Effortless Query Grading. `}
-            </div>
-            <div className="unleash-the-power">
-                Unleash the Power of Data with Ease.
-            </div>
-
-            <div className="HomePageButtons">
-                <button
-                    className="explore-animation-button"
-                    onClick={onExploreAnimationButtonClick}
-                    >
-                    <div className="explore-animations">Explore Animations</div>
-                </button>
-
-                <button
-                    className="try-out-quizzes-button"
-                    onClick={onTryOutQuizzesButtonClick}
-                    >
-                    <div className="try-out-quizzes">Try Out Quizzes</div>
-                </button>
-            </div>
-        </div>
-
-        <div className="ImageContainer1">
-            <img className="ServerDude-Image" alt="" 
-                src={MainIcon} 
+    return (
+        <>
+            <SecondHeader />
+            <img
+                className="Stacked-Waves"
+                alt=""
+                src={stackedWaves}
             />
-        </div>
+            <div className="Top-Section">
 
-    </div>
-    
-    <div className="Features-Card">
+                <div className="IHome-Page-Cont1">
 
-        <div className="Features-Card-Title">
-            Explore DataWiz Features
-        </div>
+                    <div className="your-gateway-to">
+                        {`Your Gateway to Mastering SQL Queries with Query Visualization, and Effortless Query Grading. `}
+                    </div>
+                    <div className="unleash-the-power">
+                        Unleash the Power of Data with Ease.
+                    </div>
 
-        <div className="Top-Features">
-            <button            
-                className="schema-selection-button"
-                onClick={onSchemaSelectionCardClick}
-            >
-                <img
-                    className="Schema-Selection-Image"
-                    alt=""
-                    src={SchemaIcon}
-                />
+                    <div className="HomePageButtons">
+                        <button
+                            className="explore-animation-button"
+                            onClick={onExploreAnimationButtonClick}
+                        >
+                            <div className="explore-animations">Explore Animations</div>
+                        </button>
 
-                <div className="schema-selection-for">
-                    Schema Selection for Query Animation
+                        <button
+                            className="try-out-quizzes-button"
+                            onClick={onTryOutQuizzesButtonClick}
+                        >
+                            <div className="try-out-quizzes">Try Out Quizzes</div>
+                        </button>
+                    </div>
                 </div>
 
-                <div className="Schema-Selection-Description">
-                    <p className="query-animation">
-                        Choose from 5 schemas and witness interactive SQL query animation.
-                    </p>
-                </div>
-
-            </button>
-
-            <button 
-                className="create-quiz-button" 
-                onClick={onCreateQuizButtonClick}
-            >
-
-                <img
-                    className="Create-Quizzes-Image"
-                    alt=""
-                    src={CreateQuizzIcon}
-                />
-
-                <div className="create-quiz">
-                    Create Quiz
-                </div>
-
-                <div className="Create-Quiz-Description">
-                    Never lose your valuable query animations with our efficient saving system.
-                </div>
-            </button>
-        </div>
-
-            <div className="Bottom-Features">
-                <button
-                    className="Saved-Query-Animation-button"
-                    onClick={onSavedQueryAnimationCardClick}
-                >
-                    <img
-                        className="Saved-Query-Animation-Image"
-                        alt=""
-                        src={SavedAnimationIcon}
+                <div className="ImageContainer1">
+                    <img className="ServerDude-Image" alt=""
+                        src={MainIcon}
                     />
+                </div>
 
-                    <div className="saved-query-animations">
-                        Saved Query Animations
-                    </div>
-
-                    <div className="Saved-Query-Animation-Description">
-                        Never lose your valuable query animations with our efficient saving system.
-                    </div>
-                </button>
-
-                <button 
-                    className="Saved-Quizzes-button" 
-                    onClick={onSavedQuizzesCardClick}
-                >
-                    <img
-                        className="Saved-Quizzes-Image"
-                        alt=""
-                        src={SavedQuizzesIcon}
-                    />
-                    <div className="saved-quizzes">
-                        Saved Quizzes
-                        </div>
-                    <div className="Saved-Quizzes-Description">
-                        Manage your saved quizzes in one convenient location, ensuring easy access & review.
-                    </div>
-
-                </button>
             </div>
-        </div>
-    </>
-  );
+
+            <div className="Features-Card">
+
+                <div className="Features-Card-Title">
+                    Explore DataWiz Features
+                </div>
+
+                <div className="Top-Features">
+                    <button
+                        className="schema-selection-button"
+                        onClick={onSchemaSelectionCardClick}
+                    >
+                        <img
+                            className="Schema-Selection-Image"
+                            alt=""
+                            src={SchemaIcon}
+                        />
+
+                        <div className="schema-selection-for">
+                            Schema Selection for Query Animation
+                        </div>
+
+                        <div className="Schema-Selection-Description">
+                            <p className="query-animation">
+                                Choose from 5 schemas and witness interactive SQL query animation.
+                            </p>
+                        </div>
+
+                    </button>
+
+                    <button
+                        className="create-quiz-button"
+                        onClick={onCreateQuizButtonClick}
+                    >
+
+                        <img
+                            className="Create-Quizzes-Image"
+                            alt=""
+                            src={CreateQuizzIcon}
+                        />
+
+                        <div className="create-quiz">
+                            Create Quiz
+                        </div>
+
+                        <div className="Create-Quiz-Description">
+                            Never lose your valuable query animations with our efficient saving system.
+                        </div>
+                    </button>
+                </div>
+
+                <div className="Bottom-Features">
+                    <button
+                        className="Saved-Query-Animation-button"
+                        onClick={onSavedQueryAnimationCardClick}
+                    >
+                        <img
+                            className="Saved-Query-Animation-Image"
+                            alt=""
+                            src={SavedAnimationIcon}
+                        />
+
+                        <div className="saved-query-animations">
+                            Saved Query Animations
+                        </div>
+
+                        <div className="Saved-Query-Animation-Description">
+                            Never lose your valuable query animations with our efficient saving system.
+                        </div>
+                    </button>
+
+                    <button
+                        className="Saved-Quizzes-button"
+                        onClick={onSavedQuizzesCardClick}
+                    >
+                        <img
+                            className="Saved-Quizzes-Image"
+                            alt=""
+                            src={SavedQuizzesIcon}
+                        />
+                        <div className="saved-quizzes">
+                            Saved Quizzes
+                        </div>
+                        <div className="Saved-Quizzes-Description">
+                            Manage your saved quizzes in one convenient location, ensuring easy access & review.
+                        </div>
+
+                    </button>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default InstructorHomePage;
