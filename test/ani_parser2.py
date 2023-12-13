7e5b83de60edd15e934e3aa8f3eda62a5882006f
rida-asif <rfa373@uowmail.edu.au>
2023-12-13 17:31:52 +0400

    trial

--- test/ani_parser2.py
+++ test/ani_parser2.py
@@ -337,7 +337,8 @@ def main():
     db_results, rm_keys = run(b)
     # ans = json_comp_converter(db_results, rm_keys, table_names)
     ans = json_converter(db_results, rm_keys, table_names)
-    print(ans)    
+    print(ans)
+    #print    
     
 if __name__ == "__main__":
     main()
